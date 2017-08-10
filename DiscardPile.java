bcfd8f5f00d6550502c67d95d6e37f42093c0782
daviddxu <daviddxu@cmail.carleton.ca>
2017-08-09 23:39:58 -0400

    Added pop(), isEmpty() and size() method

--- DiscardPile.java
+++ DiscardPile.java
@@ -1,20 +1,37 @@
 import java.util.Stack;
 
 public class DiscardPile{
-	/* the actual discard pile */
-	protected Stack<Card> cards = new Stack<Card>();
-	
-	/* the top card on the discard pile */
-	public Card top(){ 
-		if(cards.isEmpty()){	
-			return new Card("None","None");
-		}
-		return cards.peek(); 
-	}
-	
-	/* add a card to the discard pile */
-	public void add(Card card){
-		cards.push(card);
-	}
-	
+ /* the actual discard pile */
+ protected Stack<Card> cards = new Stack<Card>();
+ 
+ /* the top card on the discard pile */
+ public Card top(){ 
+  if(cards.isEmpty()){ 
+   return new Card("None","None");
+  }
+  return cards.peek(); 
+ }
+ 
+ public Card pop(){
+     
+   if(cards.isEmpty()){
+    return new Card("None", "None");  
+   }
+   return cards.pop();
+   
+ }
+ 
+ /* add a card to the discard pile */
+ public void add(Card card){
+  cards.push(card);
+ }
+ 
+ public int size(){
+  return cards.size(); 
+ }
+ 
+ //@Override
+ public boolean isEmpty(){
+  return cards.isEmpty();
+ }
 }
