46ef20fe8ec8e08ec3ae45d18999bb662f670182
daviddxu <daviddxu@cmail.carleton.ca>
2017-08-12 18:51:44 -0400

    Deck creation and shuffling are now static methods

--- Crazy8Game.java
+++ Crazy8Game.java
@@ -4,19 +4,9 @@ import java.util.Stack;
 import java.util.Random;
 
 public class Crazy8Game{
-
- public static void main(String[] args){
-  
-  /* create the deck */
-  Card[] deck = new Card[24];
-  int index = 0;
-  for(int r=2; r<=7; r+=1){
-   for(int s=0; s<4; s+=1){
-    deck[index++] = new Card(Card.SUITS[s], Card.RANKS[r]);
-   }
-  }
-  
-  /* shuffle the deck */
+ 
+  public static Card[] shuffle(Card[] deck){
+    
   Random rnd = new Random();
   Card swap;
   for(int i = deck.length-1; i>=0; i=i-1){
@@ -25,19 +15,41 @@ public class Crazy8Game{
    deck[pos] = deck[i];
    deck[i] = swap;
   }  
+    
+  return deck;
+  }
   
-  
+  public static Card[] deck_init(){
+  Card[] deck = new Card[52];  
+  int index = 0;
+  for(int r=2; r<=14; r+=1){
+   for(int s=0; s<4; s+=1){
+    deck[index++] = new Card(Card.SUITS[s], Card.RANKS[r]);
+   }
+  }
+  return deck;
+  }
+   
+ public static void main(String[] args){
+ 
+   Random rand = new Random();
+  /* create the deck */
+  Card[] deck = new Card[52]; 
+  deck = deck_init();  
+  /* shuffle the deck */
+  deck = shuffle(deck);
+ 
  
   
   /* players in the game */
   Player[] players = new Player[3];
-  players[0] = new BadPlayer( Arrays.copyOfRange(deck, 0, 5) );
+  players[0] = new ExtraCards( Arrays.copyOfRange(deck, 0, 5) );
   System.out.println("0 : " + Arrays.toString( Arrays.copyOfRange(deck, 0, 5))); 
-  players[1] = new BadPlayer( Arrays.copyOfRange(deck, 5, 10) );
+  players[1] = new ExtraCards( Arrays.copyOfRange(deck, 5, 10) );
   System.out.println("0 : " + Arrays.toString( Arrays.copyOfRange(deck, 5, 10))); 
-  players[2] = new BadPlayer( Arrays.copyOfRange(deck, 10, 15) );
+   players[2] = new ExtraCards( Arrays.copyOfRange(deck, 10, 15) );
   System.out.println("0 : " + Arrays.toString( Arrays.copyOfRange(deck, 10, 15))); 
-  
+   
   
   /* discard and draw piles */
   DiscardPile discardPile = new DiscardPile();
@@ -59,97 +71,65 @@ public class Crazy8Game{
 int count = 0;
 
   while( !win ){
-System.out.println("count: " + count);    
+
    
-
-while(drawPile.isEmpty() != true){
+/**TESTING ONLY STARTS**/
+/*while(drawPile.isEmpty() != true){
       discardPile.add(drawPile.pop());
-}
- //  player = (player + 1) % players.length;
-/*   System.out.println("player " + player);
-   System.out.println("draw pile    : " + drawPile.peek() );
-   System.out.println("discard pile : " + discardPile.top() );
-*/
-//   win = people.get(player).play(discardPile, drawPile, people);
-/*
-   System.out.println("draw pile   : " + drawPile.peek() );
-   System.out.println("discard pile : " + discardPile.top() );
-*/
+}*/
+/**TESTING ONLY ENDS**/
 
+   player = (player + 1) % players.length;
+//   System.out.println("player " + player);
+  // System.out.println("draw pile    : " + drawPile.peek() );
+  // System.out.println("discard pile : " + discardPile.top() );
+
+  win = people.get(player).play(discardPile, drawPile, people);
+
+//  System.out.println("draw pile   : " + drawPile.peek() );
+//   System.out.println("discard pile : " + discardPile.top() );
 
    /*shuffle start*/
-   if(drawPile.size() == 0){
-     
-   //  System.out.println("now shuffling");
+  System.out.println("draw pile size: " + drawPile.size());
+ 
+    if(drawPile.size() == 0){
+     System.out.println("recycling cards");
+   
    Stack <Card> top = new Stack <Card>();
-   Stack <Card> temp = new Stack <Card>();
  
    // saves top of discard pile
    top.add(discardPile.pop());
-   System.out.println("top: " + top);
- /*  while(discardPile.isEmpty() != true){     
-     drawPile.push(discardPile.pop());
-   }
-   */
-//  System.out.println("draw pile: ");
-  
- /* while(drawPile.isEmpty() != true){
-    
-    System.out.println(drawPile.pop());
-    
-  }*/
-   
+ //  System.out.println("top: " + top);
+ 
   Card [] shuffle_temp = new Card[discardPile.size()];
   int i = 0;
   while(discardPile.isEmpty() != true){
-
     shuffle_temp[i] = discardPile.pop();
-    i++;
-    
+    i++;  
   }
-System.out.println("printing unshuffled array");
-  for(i = 0; i < shuffle_temp.length; i++){
-    
-   System.out.println(shuffle_temp[i]); 
-    
-  }
+
+  //add top back to the discard pil
+  discardPile.add(top.pop());
+  
+  //shuffling
   Card shuffle;
    for( i = shuffle_temp.length-1; i>=0; i=i-1){
-   int position = rnd.nextInt(i+1);
+   int position = rand.nextInt(i+1);
    shuffle = shuffle_temp[position];
    shuffle_temp[position] = shuffle_temp[i];
    shuffle_temp[i] = shuffle;
   }  
-  System.out.println("priniting shuffled array");
-  for(int l = 0; l < shuffle_temp.length; l++){
-   System.out.println(shuffle_temp[l]);
-    
-  }
-  System.out.println("drawPile size : " + drawPile.size());
+  
+//copying back to the draw pile
   for(int k = 0; k < shuffle_temp.length; k++){
-    System.out.println("copying back");
-   drawPile.add(shuffle_temp[k]); 
+  drawPile.add(shuffle_temp[k]); 
   }
-  System.out.println("draw pile size: " + drawPile.size());
-  System.out.println("peeking draw pile");
-  
-  System.out.println(drawPile.peek());
-   }
-       
-//count++;
-break;
-  }
-  
-  /*shuffle end*/
 
-  
-   System.out.println("printing the shuffled list");
-   while(drawPile.isEmpty() != true){
-     
-    System.out.println(drawPile.pop()); 
+
    }
+
    
-//  System.out.println("winner is player " + player);
+  System.out.println("winner is player " + player);
   
  }
  
